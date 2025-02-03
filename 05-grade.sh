42f562297103dcf0fe5e98639131165ba05cd51a
Youssouf Konate <Ykonate1@ggc.edu>
2025-02-02 19:59:15 -0500

    temperature

--- 05-grade.sh
+++ 05-grade.sh
@@ -19,3 +19,16 @@ fi
 # that prints "it's cold" if the temperature is < 40
 # it's chilly if < 60, it's okay if < 70 and, it's hot for 
 # everything else
+
+echo "What is the temperature today?"
+read temp
+
+if [ $temp -tl 40 ]; then
+   echo "It's cold"
+elif [ $temp -tl 60 ]; then
+   echo "It's chilly"
+elif [ $temp -tl 70 ]; then
+   echo "It's okay"
+else
+   echo "It is hot"
+fi
